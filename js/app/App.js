1764533e94741d5badba8b744d09ca4c3273b869
FreAK19 <andrii.kiri4encko@yandex.ru>
2017-12-26 15:23:28 +0200

    refactoring  code

--- js/app/App.js
+++ js/app/App.js
@@ -1,10 +1,12 @@
-import Element from './Elementator';
-
 export default class Paint {
   constructor({elem, controls, palette}) {
     this.isDrawing = false;
     this.controls = controls;
-    this.palette = palette.palette.colors;
+    this.btnClear = controls.btnClear;
+    this.btnPallete = controls.btnPallete;
+    this.slider = controls.slider;
+    this.controlsContainer = controls.controlsContainer;
+    this.containerPaletteList = palette.containerPaletteList;
     this.elem = elem;
     this.ctx = elem.getContext('2d');
 
@@ -14,62 +16,13 @@ export default class Paint {
     this.init();
   }
 
-  buildControlsDOM() {
-    const fragment = document.createDocumentFragment();
-    const div = new Element('div', {class: 'controls'}).render();
-    const btnPalette = new Element('button', {id: 'new-color-button'}).render();
-    const paletteIcon = new Element('i', {class: 'material-icons'}).render();
-    const btnClear = new Element('button', {id: 'clear-canvas-button'}).render();
-    const clearIcon = new Element('i', {class: 'material-icons'}).render();
-    const input = new Element('input', {
-      type: 'range',
-      id: 'brush-size-slider',
-      min: this.controls.minValue.toString(),
-      max: this.controls.maxValue.toString(),
-      value: this.controls.lineWidth.toString()
-    }).render();
-
-    paletteIcon.textContent = 'palette';
-    clearIcon.textContent = 'clear';
-    btnPalette.appendChild(paletteIcon);
-    btnClear.appendChild(clearIcon);
-    div.append(btnPalette);
-    div.append(btnClear);
-    div.append(input);
-    fragment.appendChild(div);
-
-    this.elem.after(fragment);
-    this.btnClear = btnClear;
-    this.btnPallete = btnPalette;
-    this.slider = input;
-  }
-
-  buildPaletteDOM() {
-    const box = new Element('div', {class: 'box hidden'}).render();
-    const heading = new Element('p', {class: 'box__heading'}).render();
-    const ul = new Element('ul', {id: 'color-palette', class: 'palette'}).render();
-    const triangle = new Element('div', {class: 'triangle-down'}).render();
-    for (let i = 0; i < this.palette.length; i += 1) {
-      let y = new Element('li', {
-        class: 'palette__color-item',
-        style: `background-color: ${this.palette[i]}`,
-        'data-color': `${this.palette[i]}`
-      }).render();
-      ul.appendChild(y);
-    }
-    heading.textContent = 'Choose a color:';
-    box.appendChild(heading);
-    box.appendChild(ul);
-    box.appendChild(triangle);
-    this.elem.after(box);
-    this.listPalette = box;
+  buildTree() {
+    this.elem.after(this.containerPaletteList);
+    this.elem.after(this.controlsContainer);
   }
 
   init() {
-    if (this.controls) {
-      this.buildControlsDOM();
-      this.buildPaletteDOM();
-    }
+    this.buildTree();
     this.elem.addEventListener('mousedown', this.handleMouseDown.bind(this));
     this.elem.addEventListener('mouseup', this.handleMouseUp.bind(this));
     this.elem.addEventListener('mousemove', this.handleMouseMove.bind(this));
@@ -77,7 +30,7 @@ export default class Paint {
     this.btnClear.addEventListener('click', this.handleClearCanvas.bind(this));
     this.btnPallete.addEventListener('click', this.handleShowPalette.bind(this));
     this.slider.addEventListener('change', this.handleBrushSizeChange.bind(this));
-    this.listPalette.addEventListener('click', this.handleSelectColor.bind(this));
+    this.containerPaletteList.addEventListener('click', this.handleSelectColor.bind(this));
   }
 
   handleMouseDown(e) {
@@ -96,9 +49,8 @@ export default class Paint {
       this.ctx.strokeStyle = this.strokeStyle;
       this.ctx.beginPath();
       this.ctx.moveTo(this.x, this.y);
-      this.ctx.lineTo(e.offsetX + 0.1, e.offsetY + 0.1);
+      this.ctx.lineTo(e.offsetX, e.offsetY);
       this.ctx.stroke();
-      this.ctx.closePath();
       this.x = e.offsetX;
       this.y = e.offsetY;
     }
@@ -113,23 +65,25 @@ export default class Paint {
   }
 
   handleShowPalette() {
-    this.listPalette.classList.toggle('hidden')
+    this.containerPaletteList.classList.toggle('hidden')
   }
 
   handleBrushSizeChange(e) {
     this.lineWidth = e.target.value;
   }
 
-  handleSelectColor(e) {
+  handleSelectColor({target}) {
     const li = document.querySelectorAll('.palette__color-item');
-
-    for (let i = 0; i < li.length; i += 1) {
-      li[i].classList.remove('active');
-    }
-    const target = e.target;
     if (target.tagName === 'LI') {
-      target.classList.toggle('active');
-      this.strokeStyle = target.dataset.color;
+
+      /// remove class from all palette items
+      for (let i = 0; i < li.length; i += 1) {
+        li[i].classList.remove('active');
+      }
+
+      /// select only one palette item and add active state
+      target.classList.add('active');
+      this.strokeStyle = target.style.backgroundColor;
     }
   }
 }
--- js/app/Controls.js
+++ js/app/Controls.js
@@ -1,7 +1,48 @@
+import Element from "./Elementator";
+
 export default class Controls {
   constructor({lineWidth, minValue, maxValue}) {
     this.lineWidth = lineWidth;
     this.minValue = minValue;
     this.maxValue = maxValue;
+
+    this.btnClear = null;
+    this.btnPallete = null;
+    this.slider = null;
+    this.controlsContainer = null;
+    this.init();
   }
+
+  buildControlsDOM() {
+    const div = new Element('div', {class: 'controls'}).render();
+    const btnPalette = new Element('button', {id: 'new-color-button'}).render();
+    const paletteIcon = new Element('i', {class: 'material-icons'}).render();
+    const btnClear = new Element('button', {id: 'clear-canvas-button'}).render();
+    const clearIcon = new Element('i', {class: 'material-icons'}).render();
+    const input = new Element('input', {
+      type: 'range',
+      id: 'brush-size-slider',
+      min: this.minValue.toString(),
+      max: this.maxValue.toString(),
+      value: this.lineWidth.toString()
+    }).render();
+
+    paletteIcon.textContent = 'palette';
+    clearIcon.textContent = 'clear';
+    btnPalette.appendChild(paletteIcon);
+    btnClear.appendChild(clearIcon);
+    div.append(btnPalette);
+    div.append(btnClear);
+    div.append(input);
+
+    this.controlsContainer = div;
+    this.btnClear = btnClear;
+    this.btnPallete = btnPalette;
+    this.slider = input;
+  }
+
+  init() {
+    this.buildControlsDOM();
+  }
+
 }
